2f9a44686fb3790991e2ee82d2fb4bd5605b14bd
kauanbrtech <kauandasilva@brtech.dev>
2024-02-02 22:33:18 -0300

    in progress

--- src/app/page.js
+++ src/app/page.js
@@ -14,193 +14,56 @@ const Home = () => {
 
   const [rows, setRows] = useState([
     {
-      position: 1,
-      title: 'Inception',
-      gender: 'Ação/Ficção Científica',
-      year: '2010',
-      resolution: '1080p',
+      posicao: 1,
+      titulo: 'A Origem',
+      genero: 'Ação/Ficção Científica',
+      ano: '2010',
+      resolucao: '1080p',
       audio: 'Inglês',
-      watched: 'Sim'
+      assistido: 'Sim'
     },
     {
-      position: 2,
-      title: 'The Shawshank Redemption',
-      gender: 'Drama',
-      year: '1994',
-      resolution: '720p',
+      posicao: 2,
+      titulo: 'Um Sonho de Liberdade',
+      genero: 'Drama',
+      ano: '1994',
+      resolucao: '720p',
       audio: 'Inglês',
-      watched: 'Sim'
+      assistido: 'Sim'
     },
     {
-      position: 3,
-      title: 'The Godfather',
-      gender: 'Drama/Crime',
-      year: '1972',
-      resolution: '4K',
+      posicao: 3,
+      titulo: 'O Poderoso Chefão',
+      genero: 'Drama/Crime',
+      ano: '1972',
+      resolucao: '4K',
       audio: 'Italiano',
-      watched: 'Sim'
+      assistido: 'Sim'
     },
-    {
-      position: 4,
-      title: 'The Dark Knight',
-      gender: 'Ação/Drama',
-      year: '2008',
-      resolution: '1080p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 5,
-      title: 'Pulp Fiction',
-      gender: 'Crime/Drama',
-      year: '1994',
-      resolution: '720p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 6,
-      title: 'Schindler\'s List',
-      gender: 'Biografia/Drama',
-      year: '1993',
-      resolution: '4K',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 7,
-      title: 'Fight Club',
-      gender: 'Drama',
-      year: '1999',
-      resolution: '1080p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 8,
-      title: 'Forrest Gump',
-      gender: 'Drama/Romance',
-      year: '1994',
-      resolution: '720p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 9,
-      title: 'The Matrix',
-      gender: 'Ação/Ficção Científica',
-      year: '1999',
-      resolution: '4K',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 10,
-      title: 'The Lord of the Rings: The Fellowship of the Ring',
-      gender: 'Aventura/Fantasia',
-      year: '2001',
-      resolution: '1080p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 11,
-      title: 'Goodfellas',
-      gender: 'Biografia/Crime',
-      year: '1990',
-      resolution: '720p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 12,
-      title: 'The Silence of the Lambs',
-      gender: 'Crime/Drama',
-      year: '1991',
-      resolution: '4K',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 13,
-      title: 'The Shining',
-      gender: 'Drama/Horror',
-      year: '1980',
-      resolution: '1080p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 14,
-      title: 'The Departed',
-      gender: 'Crime/Drama',
-      year: '2006',
-      resolution: '720p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 15,
-      title: 'Gladiator',
-      gender: 'Ação/Drama',
-      year: '2000',
-      resolution: '4K',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 16,
-      title: 'The Godfather: Part II',
-      gender: 'Drama/Crime',
-      year: '1974',
-      resolution: '1080p',
-      audio: 'Italiano',
-      watched: 'Sim'
-    },
-    {
-      position: 17,
-      title: 'The Green Mile',
-      gender: 'Drama/Fantasia',
-      year: '1999',
-      resolution: '720p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 18,
-      title: 'American History X',
-      gender: 'Crime/Drama',
-      year: '1998',
-      resolution: '4K',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 19,
-      title: 'The Prestige',
-      gender: 'Drama/Mistério',
-      year: '2006',
-      resolution: '1080p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
-    {
-      position: 20,
-      title: 'Se7en',
-      gender: 'Crime/Drama',
-      year: '1995',
-      resolution: '720p',
-      audio: 'Inglês',
-      watched: 'Sim'
-    },
+    // ... (restante da lista)
   ]);
+  const [selectType, setSelectType] = useState(null);
+  const [valueFilter, setValueFilter] = useState("");
+  const typesFilter = ['titulo', 'genero', 'ano', 'resolucao', 'audio'];
+
+  const handleFilterRows = () => {
+    const newRowsFilter = rows.filter(row => {
+      if (selectType && row[selectType]) {
+        return row[selectType].toString().toLowerCase().includes(valueFilter.toLowerCase());
+      }
+      return false;
+    });
+    console.log(newRowsFilter);
+    setRows(newRowsFilter);
+  };
 
   const handleDeleteRows = (position) => {
-    const rowsDelete = rows.filter(item => item.position !== position)
+    const rowsDelete = rows.filter(item => item.posicao !== position)
     setRows(rowsDelete)
   }
-  const typesFilter = ['Titulo', 'Gênero', 'Ano', 'Resolução', 'Áudio']
-  
+
+
+
   return (
     <Box sx={{
       width: '100%',
@@ -210,35 +73,50 @@ const Home = () => {
     }}>
       <CustomContainer>
         <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-          <Grid container justifyContent="space-between" alignItems="center" spacing={2} sx={{ padding: '5px 0' }}>
+          <Grid container spacing={4}>
             <Grid item xs={12} sm={8}>
-              <Box sx={{ display: 'flex', gap: 2, width: '100%', flexDirection: { xs: 'column', sm: 'row' }, }}>
-                <TextField label="Input 1" sx={{ flex: 1 }} value={valueFilter} onChange={(e) => setValueFilter(e.target.value)} />
-                <Autocomplete
-                  disablePortal
-                  id="combo-box-demo"
-                  options={typesFilter}
-                  
-                  sx={{ flex: 1 }}
-                  renderInput={(params) => <TextField {...params} label="Movie" />}
-                />
-              </Box>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField label="Filme"
+                    fullWidth
+                    value={valueFilter}
+                    onChange={(e) => setValueFilter(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={typesFilter}
+                    fullWidth
+                    value={selectType}
+                    onChange={(e, value) => setSelectType(value)}
+                    getOptionLabel={(option) => option}
+                    renderInput={(params) => <TextField {...params} label="Filme" />}
+                  />
+                </Grid>
+              </Grid>
+
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box sx={{ display: 'flex', gap: 2, width: '100%', justifyContent: 'space-evenly' }}>
-                <ButtonComponent children={"Buscar"} />
+            <Grid item xs={12} sm={4} >
+              <Box sx={{
+                width: "100%",
+                display: "flex",
+                gap: 2,
+                justifyContent: "space-around"
+              }}>
+                <ButtonComponent children={"Buscar"} handleBuscar={handleFilterRows} />
+
                 <Button component={Link} href="/add-movie" variant="contained" sx={{
                   background: '#001928',
-                  padding: "20px",
                   color: '#fff',
+                  py: 2,
                   ":hover": {
                     background: '#001919'
                   }
                 }}>
                   <AddCircleOutlineOutlined sx={{ fill: "#fff" }} />
                 </Button>
-
-
               </Box>
             </Grid>
           </Grid>
